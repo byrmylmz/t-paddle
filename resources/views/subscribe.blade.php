31ba223146abe19f1e8f58a852827d77ba49ef58
bayram <68708205+byrmylmz@users.noreply.github.com>
2022-01-07 11:57:53 +0300

    update

--- resources/views/subscribe.blade.php
+++ resources/views/subscribe.blade.php
@@ -12,8 +12,9 @@
                 <x-paddle-button :url="$payLink" class="px-8 py-4">
                             Subscribe
                 </x-paddle-button>
-                </div>
                 
+                </div>
+
             </div>
         </div>
     </div>
